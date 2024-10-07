287fdae5f585d8d0a0a4028387d598fa568635dc
Harkit07 <harkitsinghsran9584@gmail.com>
2024-10-07 17:31:27 +0530

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let result = document.querySelector(".output");
 let clearAll = document.querySelector(".ACbtn");
 let del = document.querySelector(".del");
 let equal = document.querySelector(".equal");
-
+let p = document.querySelector(".output p");
 
 clearAll.addEventListener("click", function () {
   result.innerText = "";
@@ -12,22 +12,34 @@ clearAll.addEventListener("click", function () {
 for (btn of btns) {
   btn.addEventListener("click", function (event) {
     text = event.target.textContent;
-    result.append(text);
+    let para = document.createElement("p");
+    para.innerText = text;
+    result.insertAdjacentElement("beforeend", para);
     console.log(text);
-    console.log("button clicked");
-
-    // let question = eval(result.textContent);
-    // console.log(question);
   });
-
-};
-
-equal.addEventListener("click", function() {
-    let ans = eval(result.textContent);
-    result.innerText = ans;
-    console.log(ans);
-});
+}
 
 del.addEventListener("click", function () {
-    result.removeChild(result(length));
+  console.log("click");
+  let array = result.children;
+  let arr = array[array.length-1];
+  console.log(event);
+  // console.dir(num);
+  // result.remove(num);
+  arr.innerHTML = "";
+  console.dir(result.children);
+  // result.removeChild()
+  // if(arr.nodeName = "P") {
+  //   let item = arr
+  //   result.remove(arr.outerHTML);
+  // }
+  console.dir(arr);
+  // console.dir(result.childNodes.length);
+  // console.dir(result.lastChild.data);
+});
+
+equal.addEventListener("click", function () {
+  let ans = eval(result.textContent);
+  result.innerText = ans;
+  console.log(ans);
 });
